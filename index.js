be87c1e1d04c1e98b3b9a49a6e27d275f17bedd2
Yiyi <414956851@qq.com>
2020-04-07 20:39:07 -0400

    Adjusted for Canada store, added notification

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
-var aws = require('aws-sdk');
-var ses = new aws.SES({ region: 'us-west-2' });
-var request = require('request');
-var cheerio = require('cheerio');
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-west-2" });
+var request = require("request");
+var cheerio = require("cheerio");
+const notifier = require("node-notifier");
+//let status = false;
 
 const checkInventory = () => {
-    //Paste the url for the product you want
-    const url = "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255"
-    //Paste the cookie of your browser when you manually check website with a browser
-    const cookie = "";
-    //Set up in AWS SES following tutorial
-    const sourceEmailAddr = "xyz@gmail.com";
-    const destinationEmailAddrs = ["abc@qq.com"];
-    
-    const req = {
-        url: url,
-        headers: {
-            "Accept": "application/json, text/plain, */*",
-            "User-Agent": "axios/0.18.0",
-            "Cookie": cookie
-        }
+  //Paste the url for the product you want
+  const url =
+    // "https://www.bestbuy.ca/en-ca/product/kitchenaid-artisan-design-tilt-head-stand-mixer-5qt-325-watt-azure-blue/12917081";
+    "https://www.bestbuy.ca/en-ca/product/nintendo-switch-console-with-neon-red-blue-joy-con/13817625";
+  //Paste the cookie of your browser when you manually check website with a browser
+  const cookie = "";
+
+  const req = {
+    url: url,
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "User-Agent": "axios/0.18.0",
+      Cookie: cookie
     }
-    request(req, (error, response, body) => {
-        if (error) {
-            console.log(error);
-        }
-        console.log("Status code: " + response.statusCode);
-        const $ = cheerio.load(body);
-        const status = $('.fulfillment-add-to-cart-button div button').text().trim();
-        console.log("Inventory status: " + status);
-        console.log("time: " + (new Date).toUTCString() + "\n");
-        if (status !== "Sold Out") {
-            var params = {
-                Destination: {
-                    ToAddresses: destinationEmailAddrs
-                },
-                Message: {
-                    Body: {
-                        Text: { Data: "" }
-                    },
-                    Subject: { Data: "Nintendo Switch is available!" }
-                },
-                Source: sourceEmailAddr
-            };
-            ses.sendEmail(params, function (err, data) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("email sent")
-                    console.log(data);
-                }
-            });
-        }
+  };
+  request(req, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log("Status code: " + response.statusCode);
+
+    // Object
+    notifier.notify({
+      title: "Switch!",
+      message: "BACK IN STOCK!"
     });
-}
-console.log("Script starting...")
+
+    const $ = cheerio.load(body);
+    let status = $(".addToCartContainer_2uzan form button").is(":disabled");
+
+    console.log("Out of Stock: ", status);
+    console.log("time: " + new Date().toString() + "\n");
+    if (!status) {
+      console.log("BUY NOWWW!!!!!!!!!!!!!!!!!!!!!!!!!");
+      notifier.notify("Message");
+    }
+  });
+};
+console.log("Script starting...");
 setInterval(checkInventory, 30000);
